f5c64fbf87743c1d3325670b436229adbf3fa0dd
gadelhati <gadelha.ti@gmail.com>
2026-03-17 17:12:26 -0300

    feat: add default messages

---
+++ src/main/java/dev/forgepack/library/internal/configuration/ConfigurationMessages.java
@@ -0,0 +1,20 @@
+package dev.forgepack.library.internal.configuration;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Description;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+@Configuration
+public class ConfigurationMessages {
+
+    @Bean
+    @Description("Spring Message Resolver")
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("ValidationMessages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+}
