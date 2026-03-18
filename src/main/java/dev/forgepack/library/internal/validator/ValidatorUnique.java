5b8b0d0bd28f3b7f68b5c52b8231cad0702f696e
gadelhati <gadelha.ti@gmail.com>
2026-03-18 00:30:40 -0300

    chore: change label validation

--- src/main/java/dev/forgepack/library/internal/validator/ValidatorUnique.java
+++ src/main/java/dev/forgepack/library/internal/validator/ValidatorUnique.java
@@ -83,7 +83,7 @@ public class ValidatorUnique implements ConstraintValidator<Unique, Object> {
             context.disableDefaultConstraintViolation();
             context.buildConstraintViolationWithTemplate(
                             context.getDefaultConstraintMessageTemplate()
-                                    .replace("{label}", field)
+                                    .replace("{field}", field)
                     ).addPropertyNode(field)
                     .addConstraintViolation();
         }
