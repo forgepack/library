a576059a2a3688cd5e69a0d8e7cf7ffbafadff16
gadelhati <gadelha.ti@gmail.com>
2026-03-18 17:19:40 -0300

    feat: add swagger

---
+++ src/main/java/dev/forgepack/library/internal/configuration/ConfigurationOpenAPI.java
@@ -0,0 +1,45 @@
+package dev.forgepack.library.internal.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class ConfigurationOpenAPI {
+    @Value("${openapi.url}")
+    private String url;
+    @Value("${application.name}")
+    private String name;
+    @Value("${application.version}")
+    private String version;
+
+    public ConfigurationOpenAPI(String url, String name, String version) {
+        this.url = url;
+        this.name = name;
+        this.version = version;
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title(name)
+                        .version(version)
+                        .contact(new Contact()
+                                .name("Gadelha TI")
+                                .url("https://github.com/gadelhati/"))
+                        .description("This API exposes endpoints to manage researches.").termsOfService("https://www.forgepack.dev")
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://choosealicense.com/licenses/mit/")))
+                .servers(List.of(
+                        new Server().url(url).description("Server URL in environment")));
+    }
+}
